91c21f9fa51edc896d85595dd7e846613c08f474
ferfleitass <fleitasbaez55@gmail.com>
2024-09-25 11:33:08 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// Selecciona los elementos del DOM.
+const inputRojo = document.getElementById('rojo');
+const inputVerde = document.getElementById('verde');
+const inputAzul = document.getElementById('azul');
+
+const textoRojo = document.getElementById('texto-rojo');
+const textoVerde = document.getElementById('texto-verde');
+const textoAzul = document.getElementById('texto-azul');
+
+// Valores iniciales para definir el color RGB.
+let rojo = inputRojo.value;
+let verde = inputVerde.value;
+let azul = inputAzul.value;
+
+// Muestra los valores iniciales en los elementos <p>.
+textoRojo.textContent = rojo;
+textoVerde.textContent = verde;
+textoAzul.textContent = azul;
+
+// Función para actualizar el color de fondo de la página.
+function actualizarColor(rojo, verde, azul) {
+    let color = `rgb(${rojo},${verde},${azul})`;
+    document.body.style.backgroundColor = color;
+}
+
+// Agrega un event listener para el input de Rojo.
+inputRojo.addEventListener('change', (e) => {
+    valorRojo = e.target.value;
+    actualizarColor(rojo,verde,azul);
+});
+
+// Agrega un event listener para el input de Verde.
+inputVerde.addEventListener('change', (e) => {
+  textoVerde.textContent = verde;
+  actualizarColor(rojo,verde,azul);
+});
+
+// Agrega un event listener para el input de Azul.
+inputAzul.addEventListener('change', (e) => {
+  azul = e.target.value;
+  actualizarColor(rojo,verde,azul);
+});
